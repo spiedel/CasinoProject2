a7813befaab8309047e692e96e0c2a046bb30f28
Emma Trainer <emma.trainer@hotmail.co.uk>
2020-09-11 12:04:37 +0100

    spring setup

---
+++ src/main/java/com/example/techconnect/casinoservice/CasinoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.techconnect.casinoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techconnect/casinoservice/CasinoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techconnect.casinoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
